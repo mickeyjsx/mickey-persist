0f35f5afdfeaf6a0515d397352b8e35b9725de69
yohacai <yohacai@yohacaideMacBook-Pro.local>
2018-01-29 13:23:20 +0800

    fixed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'mickey';
 export default function(persistCombineReducers, customCombineReducers ){
     return (...args) => {
+        customCombineReducers = customCombineReducers || combineReducers;
         if( args[0]['@@MICKEY']){
             return persistCombineReducers(args[0])
         }else{
